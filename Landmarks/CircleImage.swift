d38815b1ed99c98f6b82502a673e088cd965156f
stationAstral <cougarjonez@gmail.com>
2023-12-13 19:28:09 -0800

    1.0

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by stationAstral on 12/13/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/mapview.swift
@@ -0,0 +1,18 @@
+//
+//  mapview.swift
+//  Landmarks
+//
+//  Created by stationAstral on 12/13/23.
+//
+
+import SwiftUI
+
+struct mapview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapview()
+}
